86c997810101bd8250b9073c4facb6561fc90378
nukosuke <39.nuko@gmail.com>
2016-06-13 12:05:53 +0900

    insert var to app as config

--- server/server.js
+++ server/server.js
@@ -3,53 +3,62 @@ var bodyParser = require('body-parser');
 var route      = require('./route');
 var app        = express();
 
-var Annict = require('annict').default;
-var annict = new Annict();
-
 app.set('views', __dirname+'/views');
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use('/static', express.static(__dirname+'/../public'));
 app.use(route);
 
+const config = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  ANNICT_FETCH_INTERBAL: process.env.ANNICT_FETCH_INTERBAL
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REDIRECT_URI: process.env.REDIRECT_URI,
+};
+app.set('config', config);
 
+var Annict = require('annict').default;
+var annict = new Annict();
+app.set('middlewares', {
+  annict
+});
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
-
 var models = {};
-models.User = mongoose.model('User', require('./models/user'));
-
+mongoose.connect(config.MONGODB_URI);
+const User = models.User = mongoose.model('User', require('./models/user'));
 app.set('models', models);
 
 
-var CronJob = require('cron').CronJob;
-const BATCH_INTERBAL = process.env.BATCH_INTERBAL;
-
-const User = models.User;
-
+/**
+ * Noe-Cron Job to fetch new data for all user
+ */
+const CronJob = require('cron').CronJob;
 try {
-new CronJob(`*/5 * * * * *`, function() {
-  User.find({}, function(err, users) {
-    console.log(users);
-    users.forEach(user => {
-      annict.Me.Work.get({
-        access_token: user.access_token,
-        filter_status: 'watching',
-        per_page: 10
-      })
-      .then(response => {
-        user.watching = response.works;
-        user.save();
-        console.log('update');
+  new CronJob(`* */5 * * * *`, function() {
+    User.find({}, function(err, users) {
+      users.forEach(user => {
+        annict.Me.Work.get({
+          access_token: user.access_token,
+          filter_status: 'watching',
+          per_page: 10
+        })
+        .then(response => {
+          user.watching = response.works;
+          user.save();
+        });
       });
     });
-  });
-
-}, function() {}, true, 'Asia/Tokyo');
+  },
+  () => {
+    console.log('cron job finished');
+  },
+  true,
+  'Asia/Tokyo');
 }
 catch(ex) {
   console.log(ex);
 }
 
-app.listen(process.env.PORT || 3000);
+module.exports = app;
