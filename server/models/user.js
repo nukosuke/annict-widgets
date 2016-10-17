e23911e8aceffa49e8cc1d59846e53dddc5d6455
nukosuke <nukosuke@shibuya.tech>
2016-10-17 18:03:42 +0900

    update schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
+const Widget = require('./widget');
 
 const User = new Schema({
   access_token: String,
-  watching: [{
-    title: String,
-    official_site_url: String
-  }],
+  widgets: [Widget],
   updated_at: Date
 });
 
